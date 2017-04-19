32b2c8173ae511786836102ef0875c902380914d
Greg Kops <greg@thinktopography.com>
2017-04-19 03:07:51 -0400

    stripping mycce of all non essential code

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-import maha from 'maha'
-import expenses from 'maha-expenses'
+const maha = require('maha').default
+const expenses = require('maha-expenses').default
 
 const apps = [
   expenses
 ]
 
-export default () => {
+exports.default = () => {
 
    maha({ apps }).listen(process.env.SERVER_PORT, () => {
     console.log('Server listening on port', process.env.SERVER_PORT)
