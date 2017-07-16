f38aa1f2a8bd7b577b9744dbf839a42644453a8d
Greg Kops <greg@thinktopography.com>
2017-07-16 11:28:01 -0400

    saving work on emails

--- src/db/seeds/email_templates.js
+++ src/db/seeds/email_templates.js
@@ -6,13 +6,13 @@ export default {
       code: 'activation',
       name: 'Account Activation',
       subject: 'Welcome to the Maha Platform',
-      body: '<html><body><p>Hello <%= first_name %>,</p><p>A new account has been created for you. All we need to do is make sure this is a valid email address.</p><p><a href="<%= activation_url %>">Activate Account</a></p></body></html>'
+      body: '<p>Hello <%= first_name %>,</p><p>A new account has been created for you. All we need to do is make sure this is a valid email address.</p><p>&nbsp;</p><center><table border="0" cellpadding="0" cellspacing="0"><tr><td style="background-color:#21BA45;padding:10px 20px;"><a style="font-weight:bold;color:#FFFFFF;" href="<%= activation_url %>">Activate Account</a></td></tr></table></center>'
     }, {
       team_id: 1,
       code: 'reset',
       name: 'Reset Password',
       subject: 'Reset Your Password',
-      body: '<html><body><p>Hello <%= first_name %>,</p><p>You or someone else requested password reset instructions for the account connected with this email address.</p><p>If you did not request this email, you can ignore it and no changes will be made.</p><p><a href="<%= reset_link %>">Reset Password</a></p></body></html>'
+      body: '<p>Hello <%= first_name %>,</p><p>You or someone else requested password reset instructions for the account connected with this email address.</p><p>If you did not request this email, you can ignore it and no changes will be made.</p><p><a href="<%= reset_link %>">Reset Password</a></p>'
     }
   ]
 }
