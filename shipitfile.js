35af6d19b247a8f6c46f3a870c76353d0133e136
Greg Kops <greg@thinktopography.com>
2017-09-01 15:41:25 -0400

    updating deployment

--- shipitfile.js
+++ shipitfile.js
@@ -1,32 +1,45 @@
 const config = require('./deploy')
+const deploy = require('shipit-deploy')
+const npm = require('shipit-npm')
 
 module.exports = function (shipit) {
 
   shipit.initConfig(config)
 
-  shipit.task('deploy', function () {
-    const commands = [
-      'cd /var/www/app',
-      'git pull',
-      'npm install',
-      'npm run build',
-      'npm run compile',
-      'service nginx restart'
-    ]
-    return shipit.remote(commands.join(' && '))
-  })
+  deploy(shipit)
 
-  shipit.task('reset', function () {
-    const commands = [
-      'cd /var/www/app',
-      'NODE_ENV=production ./bin/platform.js ccetc:import',
-      'NODE_ENV=production ./bin/platform.js db:rollback',
-      'NODE_ENV=production ./bin/platform.js db:migrate',
-      'NODE_ENV=production ./bin/platform.js db:seed',
-      'NODE_ENV=production ./bin/platform.js db:imports',
-      'service nginx restart'
-    ]
-    return shipit.remote(commands.join(' && '))
-  })
+  // npm(shipit)
+
+  shipit.on('published', function () {
+    shipit.task('restart server', function () {
+      return shipit.remote('touch /var/www/maha/staging/current/tmp/restart.txt')
+    })
+
+  });
+
+  // shipit.task('deploy', function () {
+  //   const commands = [
+  //     'cd /var/www/'+app+'/',
+  //     'git pull',
+  //     'npm install',
+  //     'npm run build',
+  //     'npm run compile',
+  //     'service nginx restart'
+  //   ]
+  //   return shipit.remote(commands.join(' && '))
+  // })
+  //
+  // shipit.task('reset', function () {
+  //   const commands = [
+  //     'cd /var/www/app',
+  //     'NODE_ENV=production ./bin/platform.js ccetc:import',
+  //     'NODE_ENV=production ./bin/platform.js db:rollback',
+  //     'NODE_ENV=production ./bin/platform.js db:migrate',
+  //     'NODE_ENV=production ./bin/platform.js db:seed',
+  //     'NODE_ENV=production ./bin/platform.js db:imports',
+  //     'service nginx restart'
+  //   ]
+  //   return shipit.remote(commands.join(' && '))
+  // })
 
 }
