75d0a21227fa5ffaebced3ed84c3c6c379d7b1c2
Greg Kops <greg@thinktopography.com>
2017-09-02 08:12:30 -0400

    update logs for worker

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
       name: 'worker',
       script: './dist/worker.js',
       cwd: process.env.PWD + '/current',
-      error_file: process.env.PWD + '/logs/worker.err.log',
-      out_file: process.env.PWD + '/logs/worker.out.log',
+      error_file: process.env.PWD + '/current/logs/worker.err.log',
+      out_file: process.env.PWD + '/current/logs/worker.out.log',
       exec_mode: 'fork_mode'
     }
   ]
