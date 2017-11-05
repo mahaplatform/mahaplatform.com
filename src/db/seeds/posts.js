e2858344e56ca25ac6091fc30796eb3564aaca5d
Greg Kops <greg@thinktopography.com>
2017-11-05 01:42:44 -0500

    refining lists for mobile

--- src/db/seeds/posts.js
+++ src/db/seeds/posts.js
@@ -7,7 +7,7 @@ const postFixtures = new Fixtures({
       id: 1,
       team_id: 1,
       user_id: 79,
-      text: '<h2>Welcome to Maha</h2><p>After almost a full year of development, the Maha Platform is finally live! Thank you to everyone who has assisted with design, brainstorming, testing, feedback, and emotional support. I\'m looking forward to continuing the development of this platform for the benefit of the whole cooperative extension!</p>',
+      text: '<h2>Welcome to Maha</h2><p>After almost a full year of development, the Maha Platform is finally live! Thank you to everyone who has assisted with brainstorming, design, testing, feedback, and moral support. I\'m looking forward to continuing the development of this platform for the benefit of the whole cooperative extension!</p>',
       created_at: '2017-11-03T21:30:00.955Z'
     }
   ]
