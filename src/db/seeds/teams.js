04851140d78fbc631cad57713678e83a7a98aaae
Greg Kops <greg@thinktopography.com>
2018-05-01 02:27:10 -0400

    implementing dropbox api

--- src/db/seeds/teams.js
+++ src/db/seeds/teams.js
@@ -2,8 +2,6 @@ import { Fixtures } from 'maha'
 import fs from 'fs'
 import path from 'path'
 
-const email_template = fs.readFileSync(path.resolve(__dirname,'..','..','..','node_modules','maha','src','platform','templates','email.ejs'), 'utf8')
-
 const teamFixtures = new Fixtures({
   tableName: 'maha_teams',
   records: [
@@ -12,7 +10,6 @@ const teamFixtures = new Fixtures({
       title: 'CCE Tompkins',
       subdomain: 'ccetompkins',
       color: 'red',
-      email_template,
       logo_id: 1
     }
   ]
