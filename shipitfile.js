7e236f174f21814d24f438ed4a52f1a93afc8a6e
Greg Kops <greg@thinktopography.com>
2018-06-21 10:56:07 -0400

    update maha

--- shipitfile.js
+++ shipitfile.js
@@ -1,5 +1,5 @@
 const config = require('./config/deploy')
-const maha = require('shipit-maha')
+const maha = require('./node_modules/maha/node_modules/shipit-maha')
 
 module.exports = function (shipit) {
 
