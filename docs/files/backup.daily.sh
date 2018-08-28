0262f543f6a2f4d6e8be89913cdae6441c7033c8
Greg Kops <greg@thinktopography.com>
2018-08-28 12:13:09 -0400

    removing maha-tasks and maha-news

--- docs/files/backup.daily.sh
+++ docs/files/backup.daily.sh
@@ -3,6 +3,6 @@
 backupfile="daily-"`date "+%w"`".sql.gz"
 backuproot="/var/lib/pgsql/9.6/backups"
 mkdir -p $backuproot
-pg_dump -h localhost -U maha maha | gzip > $backuproot/backup.sql.gz
+pg_dump -h db.mahaplatform.com -U maha maha | gzip > $backuproot/backup.sql.gz
 s3cmd put backup.sql.gz s3://data.mahaplatform.com/$backupfile
-rm -f $backupfile
+rm -rf $backuproot/backup.sql.gz
