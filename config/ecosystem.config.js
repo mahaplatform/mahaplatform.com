001ea3b9c2902699eb2cd98fcfcd51d236ca307e
Greg Kops <greg@thinktopography.com>
2018-10-16 16:12:29 +0300

    updating ecosystem config

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -3,16 +3,16 @@ module.exports = {
     {
       name: 'worker_'+process.env.NODE_ENV,
       script: './dist/worker.js',
-      cwd: process.env.PWD + '/current',
-      error_file: process.env.PWD + '/current/logs/worker.err.log',
-      out_file: process.env.PWD + '/current/logs/worker.out.log',
+      cwd: process.env.PWD,
+      error_file: process.env.PWD + '/logs/worker.err.log',
+      out_file: process.env.PWD + '/logs/worker.out.log',
       exec_mode: 'fork_mode'
     }, {
       name: 'cron_'+process.env.NODE_ENV,
       script: './dist/cron.js',
-      cwd: process.env.PWD + '/current',
-      error_file: process.env.PWD + '/current/logs/cron.err.log',
-      out_file: process.env.PWD + '/current/logs/cron.out.log',
+      cwd: process.env.PWD,
+      error_file: process.env.PWD + '/logs/cron.err.log',
+      out_file: process.env.PWD + '/logs/cron.out.log',
       exec_mode: 'fork_mode'
     }
   ]
