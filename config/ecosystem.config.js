474618efbdf05d79984c15501354bb551363b87c
Greg Kops <greg@thinktopography.com>
2018-12-17 23:50:26 -0500

    new packages

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -2,14 +2,16 @@ module.exports = {
   apps: [
     {
       name: 'worker_'+process.env.NODE_ENV,
-      script: './dist/worker.js',
+      script: './current/dist/worker.js',
+      instances: 4,
       cwd: process.env.PWD,
       error_file: process.env.PWD + '/logs/worker.err.log',
       out_file: process.env.PWD + '/logs/worker.out.log',
       exec_mode: 'fork_mode'
     }, {
       name: 'cron_'+process.env.NODE_ENV,
-      script: './dist/cron.js',
+      script: './current/dist/cron.js',
+      instances: 1,
       cwd: process.env.PWD,
       error_file: process.env.PWD + '/logs/cron.err.log',
       out_file: process.env.PWD + '/logs/cron.out.log',
---
+++ docs/files/.bash_profile
@@ -0,0 +1,14 @@
+# .bash_profile
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+fi
+
+# User specific environment and startup programs
+
+PATH=$PATH:$HOME/bin
+
+source scl_source enable devtoolset-7
+
+export PATH
