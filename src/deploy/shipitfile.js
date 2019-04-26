62f931fd74ba7ad9abdad197781d386dd0da03f1
greg@thinktopography.com <greg@thinktopography.com>
2019-04-26 10:48:50 -0400

    replace rsa key

--- src/deploy/shipitfile.js
+++ src/deploy/shipitfile.js
@@ -14,8 +14,8 @@ const shipitfile = (shipit) => {
   shipit.initConfig({
     default: {
       deployTo: '/var/www/maha/production',
-      // key: '~/.ssh/id_rsa_0d79bf2b27c217a2ac17896617668a50',
-      key: '~/.ssh/mahaplatform',
+      key: '~/.ssh/id_rsa_0d79bf2b27c217a2ac17896617668a50',
+      // key: '~/.ssh/mahaplatform',
       strict: 'no'
     },
     production: {
