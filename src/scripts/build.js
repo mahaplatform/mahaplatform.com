a491f9997f4a127a0e13ad025e04d488a647c3c6
greg@thinktopography.com <greg@thinktopography.com>
2019-05-23 10:38:31 -0400

    fixing build

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -47,13 +47,11 @@ const listItems = (root) => fs.readdirSync(root).reduce((items, item) => [
 ], []).filter(item => {
   if(item.src.match(/\.git/)) return false
   if(item.src.match(/\.DS_Store/)) return false
+  if(item.src.match(/_test.js$/)) return false
   if(item.src.match(/apps\/[^/]*\/help/)) return false
   if(item.src.match(/apps\/[^/]*\/db/)) return false
-  if(item.src.match(/apps\/[^/]*\/admin\/alerts/)) return false
   if(item.src.match(/apps\/[^/]*\/admin\/badges/)) return false
   if(item.src.match(/apps\/[^/]*\/admin\/components/)) return false
-  if(item.src.match(/apps\/[^/]*\/admin\/notifications/)) return false
-  if(item.src.match(/apps\/[^/]*\/admin\/rights/)) return false
   if(item.src.match(/apps\/[^/]*\/admin\/roots/)) return false
   if(item.src.match(/apps\/[^/]*\/admin\/routes/)) return false
   if(item.src.match(/apps\/[^/]*\/admin\/tokens/)) return false
