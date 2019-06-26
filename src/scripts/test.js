8ba60b8dd462f875429b4d148828aba142ba9928
greg@thinktopography.com <greg@thinktopography.com>
2019-06-26 19:34:26 -0400

    path tests

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,6 +1,6 @@
 import '../web/core/services/environment'
 import knex from '../web/core/services/knex'
-import { setup } from '../web/core/db/utils'
+import { setup } from '../web/core/scripts/db/utils'
 import Mocha from 'mocha'
 import glob from 'glob'
 
