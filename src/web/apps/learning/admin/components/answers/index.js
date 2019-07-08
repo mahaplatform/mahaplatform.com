084e68143b208ab75c3d24d3473c6078627cfc91
greg@thinktopography.com <greg@thinktopography.com>
2019-07-08 09:45:21 -0400

    work on answers

--- src/web/apps/learning/admin/components/answers/index.js
+++ src/web/apps/learning/admin/components/answers/index.js
@@ -1,11 +1,13 @@
 import { Factory } from 'redux-rubberstamp'
 import reducer from './reducer'
 import Answers from './answers'
+import * as selectors from './selectors'
 import * as actions from './actions'
 
 export default Factory({
-  namespace: 'answers',
+  namespace: 'learning.answers',
   component: Answers,
   reducer,
+  selectors,
   actions
 })
