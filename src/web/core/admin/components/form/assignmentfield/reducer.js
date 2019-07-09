9028858187f04a91900a65605920acda60cba241
greg@thinktopography.com <greg@thinktopography.com>
2019-07-08 20:29:53 -0400

    saving for now

--- src/web/core/admin/components/form/assignmentfield/reducer.js
+++ src/web/core/admin/components/form/assignmentfield/reducer.js
@@ -1,17 +1,5 @@
 const INITIAL_STATE = {
-  assignments: [{
-    group_id: 1,
-    user_id: null,
-    is_everyone: false
-  },{
-    group_id: null,
-    user_id: 79,
-    is_everyone: false
-  },{
-    group_id: null,
-    user_id: null,
-    is_everyone: true
-  }],
+  assignments: [],
   status: 'pending',
   unassigned: []
 }
