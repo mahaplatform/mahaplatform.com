78dbcff4f48a564ebd7a82dd8fea8ee89eea335e
greg@thinktopography.com <greg@thinktopography.com>
2019-07-16 14:27:57 -0400

    saving work on tokens

--- src/web/core/admin/components/form/select/select.js
+++ src/web/core/admin/components/form/select/select.js
@@ -1,6 +1,6 @@
-import Token from '../../token'
-import Format from '../../format'
+import Token from '../../../tokens/token'
 import PropTypes from 'prop-types'
+import Format from '../../format'
 import React from 'react'
 import _ from 'lodash'
 
--- src/web/core/admin/components/token/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Token = ({ value }) => (
-  <div className="token">
-    { value }
-  </div>
-)
-
-export default Token
