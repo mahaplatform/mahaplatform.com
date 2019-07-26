aae1b2e69ed09981aabe49aa069885c33870398a
greg@thinktopography.com <greg@thinktopography.com>
2019-07-25 22:46:33 -0400

    editing splits on expenses

--- src/web/apps/expenses/admin/views/reimbursements/details.js
+++ src/web/apps/expenses/admin/views/reimbursements/details.js
@@ -40,7 +40,11 @@ const Details = ({ reimbursement, commentUrl }) => {
   list.items = [
     requiredField('User', reimbursement, 'user.full_name'),
     { label: 'Date', content: reimbursement.date, format: 'date' },
-    requiredField('Vendor', reimbursement, 'vendor.name', { content: reimbursement, format: CompactVendorToken })
+    requiredField('Vendor', reimbursement, 'vendor.name', { content: reimbursement, format: CompactVendorToken }),
+    { label: 'Project', content: reimbursement.line_items[0].project.title },
+    { label: 'Expense Type', content: reimbursement.line_items[0].expense_type.title },
+    { label: 'Description', content: reimbursement.line_items[0].description },
+    { label: 'Amount', content: reimbursement.line_items[0].amount }
   ]
   if(reimbursement.line_items.length > 1) {
     list.items.push({ component: <LineItemsToken line_items={ reimbursement.line_items } type="reimbursement" active={ reimbursement.id } /> })
