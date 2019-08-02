6c28819ae1532902799c25fb56ffb567986d399e
greg@thinktopography.com <greg@thinktopography.com>
2019-08-02 13:59:58 -0400

    fixing tests

--- src/web/apps/expenses/admin/user_tasks.js
+++ src/web/apps/expenses/admin/user_tasks.js
@@ -1,15 +1,13 @@
 import Memberships from './views/memberships/projects'
 import React from 'react'
 
-const userTasks = (user, context) => {
-  return [
-    {
-      label: 'Manage Projects',
-      rights: ['expenses:manage_configuration'],
-      show: user.is_active,
-      modal: <Memberships user_id={ user.id } />
-    }
-  ]
-}
+const userTasks = (user, context) => [
+  {
+    label: 'Manage Projects',
+    rights: ['expenses:manage_configuration'],
+    show: user.is_active,
+    modal: <Memberships user_id={ user.id } />
+  }
+]
 
 export default userTasks
