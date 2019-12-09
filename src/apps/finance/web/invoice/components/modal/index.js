55b96a5c9c4eac283ddd1834d52427e27fc9ab0c
Greg Kops <greg@thinktopography.com>
2019-12-09 12:49:40 -0500

    replacing invoice modal

--- src/apps/finance/web/invoice/components/modal/index.js
+++ src/apps/finance/web/invoice/components/modal/index.js
@@ -1,11 +1,80 @@
-import { Factory } from 'redux-rubberstamp'
-import reducer from './reducer'
-import modal from './modal'
-import * as actions from './actions'
+import { CSSTransition } from 'react-transition-group'
+import PropTypes from 'prop-types'
+import Stack from '../stack'
+import React from 'react'
 
-export default Factory({
-  namespace: 'maha.modal',
-  component: modal,
-  reducer,
-  actions
-})
+class Modal extends React.Component {
+
+  static childContextTypes = {
+    modal: PropTypes.object
+  }
+
+  static propTypes = {
+    children: PropTypes.any
+  }
+
+  state = {
+    cards: []
+  }
+
+  render() {
+    const { cards } = this.state
+    const { children } = this.props
+    return ([
+      children,
+      <CSSTransition key="maha-modal-overlay" in={ cards.length > 0 } classNames="expanded" timeout={ 500 } mountOnEnter={ true } unmountOnExit={ true }>
+        <div className="maha-modal-overlay" onClick={this._handleClose.bind(this)} />
+      </CSSTransition>,
+      <CSSTransition key="maha-modal-window" in={ cards.length > 0 } classNames="expanded" timeout={ 500 } mountOnEnter={ true } unmountOnExit={ true }>
+        <div className="maha-modal-window">
+          <Stack { ...this._getStack() } />
+        </div>
+      </CSSTransition>
+    ])
+  }
+
+  getChildContext() {
+    return {
+      modal: {
+        close: this._handleClose.bind(this),
+        open: this._handleOpen.bind(this),
+        pop: this._handlePop.bind(this),
+        push: this._handlePush.bind(this)
+      }
+    }
+  }
+
+  _getStack() {
+    const { cards } = this.state
+    return {
+      cards,
+      slideFirst: false
+    }
+  }
+
+  _handleClose() {
+    this._handlePop()
+  }
+
+  _handleOpen(component) {
+    this._handlePush(component)
+  }
+
+  _handlePop(index = -1) {
+    this.setState({
+      cards:this.state.cards.slice(0, index)
+    })
+  }
+
+  _handlePush(component, props) {
+    this.setState({
+      cards: [
+        ...this.state.cards,
+        { component, props }
+      ]
+    })
+  }
+
+}
+
+export default Modal
