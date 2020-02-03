971f8bd444043eef7b8e8a5c09e665f63ed243a7
Greg Kops <greg@thinktopography.com>
2020-02-03 18:22:42 -0500

    text file

--- src/core/lib/express/deeplink/merchantid.js
+++ src/core/lib/express/deeplink/merchantid.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import fs from 'fs'
 
-const merchantid = fs.readFileSync(path.join(__dirname, 'apple-developer-merchantid-domain-association'), 'utf8')
+const merchantid = fs.readFileSync(path.join(__dirname, 'apple-developer-merchantid-domain-association.txt'), 'utf8')
 
 const merchantidRoute = async (req, res) => {
 
