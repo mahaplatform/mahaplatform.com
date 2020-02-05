1715e144bb2195c0df141c277d75e859ffdba8c8
Greg Kops <greg@thinktopography.com>
2020-02-05 17:22:22 -0500

    fix grouping

--- src/apps/drive/services/files.js
+++ src/apps/drive/services/files.js
@@ -100,7 +100,7 @@ export const createFile = async (req, params) => {
     await Promise.map(accesses, async access => await Access.forge({
       team_id: req.team.get('id'),
       code: file.get('code'),
-      grouping: access.get('grouping'),
+      grouping_id: access.get('grouping_id'),
       group_id: access.get('group_id'),
       user_id: access.get('user_id'),
       access_type_id: access.get('access_type_id') === 1 ? 2 : access.get('access_type_id')
