12faf84916a085e2cfa7a73409ca349de09cf28e
Greg Kops <greg@thinktopography.com>
2020-02-10 11:03:25 -0500

    sending invoice

--- src/apps/finance/admin/api/invoices/send.js
+++ src/apps/finance/admin/api/invoices/send.js
@@ -1,9 +1,7 @@
-import InvoiceSerializer from '../../../serializers/invoice_serializer'
 import { activity } from '../../../../../core/services/routes/activities'
+import sendInvoiceQueue from '../../../queues/send_invoice_queue'
 import { audit } from '../../../../../core/services/routes/audit'
 import socket from '../../../../../core/services/routes/emitter'
-import { send_email } from '../../../../maha/services/emails'
-import Sender from '../../../../crm//models/sender'
 import Invoice from '../../../models/invoice'
 
 const sendRoute = async (req, res) => {
@@ -22,26 +20,13 @@ const sendRoute = async (req, res) => {
     message: 'Unable to load invoice'
   })
 
-  const sender = await Sender.query(qb => {
-    qb.where('program_id', invoice.get('program_id'))
-    qb.where('is_verified', true)
-    qb.where('id', req.body.sender_id)
-  }).fetch({
-    transacting: req.trx
-  })
-
-  await send_email(req, {
-    team_id: req.team.get('id'),
-    from: sender.get('rfc822'),
+  await sendInvoiceQueue.enqueue (req, {
+    invoice_id: invoice.get('id'),
+    sender_id: req.body.sender_id,
     reply_to: req.body.reply_to,
     to: req.body.to,
-    template: 'finance:invoice',
     subject: req.body.subject,
-    maha: false,
-    data: {
-      message: req.body.message,
-      invoice: InvoiceSerializer(req, invoice)
-    }
+    message: req.body.message
   })
 
   await audit(req, {
---
+++ src/apps/finance/queues/send_invoice_queue.js
@@ -0,0 +1,58 @@
+import InvoiceSerializer from '../serializers/invoice_serializer'
+import { send_email } from '../../maha/services/emails'
+import Queue from '../../../core/objects/queue'
+import Sender from '../../crm/models/sender'
+import Invoice from '../models/invoice'
+
+const processor = async (job, trx) => {
+
+  const { invoice_id, sender_id, reply_to, to, subject, message } = job.data
+
+  const invoice = await Invoice.query(qb => {
+    qb.select('finance_invoices.*','finance_invoice_details.*')
+    qb.innerJoin('finance_invoice_details', 'finance_invoice_details.invoice_id', 'finance_invoices.id')
+    qb.where('finance_invoices.id', invoice_id)
+  }).fetch({
+    withRelated: ['customer','coupon','line_items.product','payments','program.logo','team'],
+    transacting: trx
+  })
+
+  const req = {
+    team: invoice.related('team'),
+    trx
+  }
+
+  const sender = await Sender.query(qb => {
+    qb.where('program_id', invoice.get('program_id'))
+    qb.where('is_verified', true)
+    qb.where('id', sender_id)
+  }).fetch({
+    transacting: req.trx
+  })
+
+  await send_email(req, {
+    team_id: req.team.get('id'),
+    from: sender.get('rfc822'),
+    reply_to,
+    to,
+    template: 'finance:invoice',
+    subject,
+    maha: false,
+    data: {
+      invoice: InvoiceSerializer(req, invoice),
+      message
+    }
+  })
+
+}
+
+const failed = async (job, err) => {}
+
+const SendInvoiceQueue = new Queue({
+  name: 'send_invoice',
+  enqueue: async (req, job) => job,
+  processor,
+  failed
+})
+
+export default SendInvoiceQueue
