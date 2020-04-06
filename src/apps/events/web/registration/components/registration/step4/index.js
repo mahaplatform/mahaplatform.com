b35738b169e563f9ff9580394e2a74a5e57387d8
Greg Kops <greg@thinktopography.com>
2020-04-06 08:08:41 -0400

    moving paymentmethod

--- src/apps/events/web/registration/components/registration/step4/index.js
+++ src/apps/events/web/registration/components/registration/step4/index.js
@@ -1,16 +1,7 @@
+import PaymentMethodField from '../../paymentmethodfield'
 import { Button, Loader } from 'maha-client'
 import PropTypes from 'prop-types'
 import React from 'react'
-import _ from 'lodash'
-
-const methods = [
-  { label: 'Credit Card', name: 'card', component: <div>Card</div> },
-  { label: 'Bank Account', name: 'ach', component: <div>ACH</div> },
-  { label: 'PayPal', name: 'paypal', component: <div>PayPal</div> },
-  { label: 'Google Pay', name: 'googlepay', component: <div>GooglePay</div> },
-  { label: 'ApplePay', name: 'applepay', component: <div>ApplePay</div> },
-  { label: 'Pay at the door', name: 'door', component: <div>Pay at the door</div> }
-]
 
 class Step4 extends React.Component {
 
@@ -25,8 +16,7 @@ class Step4 extends React.Component {
   _handleNext = this._handleNext.bind(this)
 
   render() {
-    const methods = this._getMethods()
-    const { status } = this.props
+    const { event, status } = this.props
     return (
       <div className="registration-panel">
         { status !== 'pending' &&
@@ -38,18 +28,7 @@ class Step4 extends React.Component {
           <div className="registration-panel-body">
             <div className="registration-panel-content">
               <div className="registration-step4">
-                <div className="registration-methods">
-                  { methods.map((method, index) => (
-                    <div className="registration-method" key={`method_${index}`} onClick={ this._handleMethod.bind(this, method.component) }>
-                      <div className="registration-method-mark">
-                        <img src={`/admin/images/payments/${method.name}-mark.png`} />
-                      </div>
-                      <div className="registration-method-label">
-                        { method.label }
-                      </div>
-                    </div>
-                  )) }
-                </div>
+                <PaymentMethodField event={ event }/>
               </div>
             </div>
           </div>
@@ -74,14 +53,6 @@ class Step4 extends React.Component {
       disabled: status !== 'pending',
       handler: this._handleBack
     }
-  }
-
-  _getMethods() {
-    const { event } = this.props
-    const { payment_methods } = event.payment_config
-    return payment_methods.map(name => {
-      return _.find(methods, { name })
-    })
   }
 
   _getNext() {
