c9809748ae6ebac03a559c67fba3321f12f7c57c
Greg Kops <greg@thinktopography.com>
2020-04-14 09:04:55 -0400

    updates for cornell metadata

--- src/apps/maha/admin/server/auth/cornell.js
+++ src/apps/maha/admin/server/auth/cornell.js
@@ -2,16 +2,23 @@ import { getState, loadUserByEmail, result } from './utils'
 import { Strategy as SAMLStrategy } from 'passport-saml'
 import passport from 'passport'
 
+const config = {
+  cert: process.env.CORNELL_CERT,
+  issuer: process.env.CORNELL_ISSUER,
+  entryPoint: process.env.CORNELL_ENTRY_POINT,
+  privateCert: process.env.CORNELL_PRIVATE_KEY || null,
+  decryptionPvk: process.env.CORNELL_PRIVATE_CERT || null,
+  callbackUrl: `${process.env.WEB_HOST}/admin/auth/cornell`,
+  acceptedClockSkewMs: 300000
+}
+
 export const cornell = async (req, res, next) => {
 
   const state = getState(req)
 
   const strategy = new SAMLStrategy({
-    cert: process.env.CORNELL_CERT,
-    issuer: process.env.CORNELL_ISSUER,
-    entryPoint: process.env.CORNELL_ENTRY_POINT,
-    callbackUrl: `${process.env.WEB_HOST}/admin/auth/cornell?RelayState=${state}`,
-    acceptedClockSkewMs: 300000
+    ...config,
+    callbackUrl: `${process.env.WEB_HOST}/admin/auth/cornell?RelayState=${state}`
   }, (profile, done) => {
     loadUserByEmail(req, profile.email, done)
   })
@@ -26,17 +33,11 @@ export const cornell = async (req, res, next) => {
 
 export const metadata = async (req, res) => {
 
-  const strategy = new SAMLStrategy({
-    cert: process.env.CORNELL_CERT,
-    issuer: process.env.CORNELL_ISSUER,
-    entryPoint: process.env.CORNELL_ENTRY_POINT,
-    callbackUrl: `${process.env.WEB_HOST}/admin/auth/cornell`,
-    acceptedClockSkewMs: 300000
-  }, (profile, done) => {
-    loadUserByEmail(req, profile.email, done)
-  })
+  const strategy = new SAMLStrategy(config, () => {})
 
-  res.status(200).type('application/xml').send(strategy.generateServiceProviderMetadata())
+  const metadata = strategy.generateServiceProviderMetadata(process.env.CORNELL_PRIVATE_KEY, process.env.CORNELL_PRIVATE_CERT)
+
+  res.status(200).type('application/xml').send(metadata)
 
 }
 
