0a5a7ea93c092c856a40dfa7bf51e3cc59314aac
Greg Kops <greg@thinktopography.com>
2020-04-14 20:15:36 -0400

    variable separator

--- src/scripts/certs.js
+++ src/scripts/certs.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
 
-const parse = (filepath) => {
+const parse = (filepath, separator) => {
   const data = fs.readFileSync(filepath, 'utf8')
-  return data.split(/\n/).slice(1).slice(0, -2).join(' ')
+  return data.split(/\n/).slice(1).slice(0, -2).join(separator)
 }
 
 const output = []
-output.push(`CORNELL_DECRYPTION_KEY="${parse('saml-decryption.mahaplatform.com.key')}"`)
-output.push(`CORNELL_DECRYPTION_CERT="${parse('saml-decryption.mahaplatform.com.crt')}"`)
-output.push(`CORNELL_SIGNING_KEY="${parse('saml-signing.mahaplatform.com.key')}"`)
-output.push(`CORNELL_SIGNING_CERT="${parse('saml-signing.mahaplatform.com.crt')}"`)
+output.push(`CORNELL_DECRYPTION_KEY="${parse('saml-decryption.mahaplatform.com.key', '')}"`)
+output.push(`CORNELL_DECRYPTION_CERT="${parse('saml-decryption.mahaplatform.com.crt', ' ')}"`)
+output.push(`CORNELL_SIGNING_KEY="${parse('saml-signing.mahaplatform.com.key', '')}"`)
+output.push(`CORNELL_SIGNING_CERT="${parse('saml-signing.mahaplatform.com.crt', ' ')}"`)
 
 fs.writeFileSync('certenv', output.join('\n'))
