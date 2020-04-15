bb72678965da8bd4be0df03493411124c60f3d1e
Greg Kops <greg@thinktopography.com>
2020-04-15 00:28:02 -0400

    update script to include header and footer for decryption key

--- src/scripts/certs.js
+++ src/scripts/certs.js
@@ -1,16 +1,18 @@
 const fs = require('fs')
 
-const parse = (filepath, separator) => {
+const parse = (filepath, separator, slice) => {
   const data = fs.readFileSync(filepath, 'utf8')
-  return data.split(/\n/).slice(1).slice(0, -2).join(separator)
+  const parts = data.split(/\n/)
+  const sliced = slice ? parts.slice(1).slice(0, -2) : parts
+  return sliced.join(separator)
 }
 
 const output = []
-output.push(`CORNELL_DECRYPTION_KEY="${parse('saml-decryption.mahaplatform.com.key', '')}"`)
-output.push(`CORNELL_DECRYPTION_CERT="${parse('saml-decryption.mahaplatform.com.crt', ' ')}"`)
-output.push(`CORNELL_SIGNING_KEY="${parse('saml-signing.mahaplatform.com.key', '')}"`)
-output.push(`CORNELL_SIGNING_CERT="${parse('saml-signing.mahaplatform.com.crt', ' ')}"`)
-output.push(`CORNELL_CERT="${parse('saml-cornell.prod.crt', ' ')}"`)
+output.push(`CORNELL_DECRYPTION_KEY="${parse('saml-decryption.mahaplatform.com.key', '\\n', false)}"`)
+output.push(`CORNELL_DECRYPTION_CERT="${parse('saml-decryption.mahaplatform.com.crt', ' ', true)}"`)
+output.push(`CORNELL_SIGNING_KEY="${parse('saml-signing.mahaplatform.com.key', '', true)}"`)
+output.push(`CORNELL_SIGNING_CERT="${parse('saml-signing.mahaplatform.com.crt', ' ', true)}"`)
+output.push(`CORNELL_CERT="${parse('saml-cornell.prod.crt', ' ', true)}"`)
 output.push('CORNELL_ENTRY_POINT=https://shibidp.cit.cornell.edu/idp/profile/SAML2/Redirect/SSO')
 
 fs.writeFileSync('certenv', output.join('\n'))
