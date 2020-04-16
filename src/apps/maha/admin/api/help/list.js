5a0209937656de8dfa5de13be6f7f5725c044a77
Greg Kops <greg@thinktopography.com>
2020-04-15 23:03:42 -0400

    delete legacy help and only show file you have access to

--- src/apps/maha/admin/api/help/list.js
+++ src/apps/maha/admin/api/help/list.js
@@ -4,7 +4,10 @@ import HelpArticle from '../../../models/help_article'
 const listRoute = async (req, res) => {
 
   const articles = await HelpArticle.filterFetch({
-    scope: (qb) => {},
+    scope: (qb) => {
+      qb.innerJoin('maha_roles_apps', 'maha_roles_apps.app_id', 'maha_help_articles.app_id')
+      qb.joinRaw('inner join maha_users_roles on maha_users_roles.role_id=maha_roles_apps.role_id and maha_users_roles.user_id=?', req.user.get('id'))
+    },
     filter: {
       params: req.query.$filter,
       search: ['title']
