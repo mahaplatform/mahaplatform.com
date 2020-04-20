a836e47e1b7b4c1b1801a3d3166f478dff1ccd3a
Greg Kops <greg@thinktopography.com>
2020-04-20 07:29:21 -0400

    remove legacy

--- servers/legacy/db/backup.hourly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-backupfile="hourly-"`date "+%l"`".sql.gz"
-backuproot="/var/lib/pgsql/9.6/backups"
-mkdir -p $backuproot
-pg_dump -h db.mahaplatform.com -U maha maha | gzip > $backuproot/backup.sql.gz
-s3cmd put backup.sql.gz s3://data.mahaplatform.com/$backupfile
-rm -rf $backuproot/backup.sql.gz
