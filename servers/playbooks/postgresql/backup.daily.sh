d7ad084a5856be678707f7a7c636bbd5fe39f5e2
Greg Kops <greg@thinktopography.com>
2020-04-23 17:38:43 -0400

    adding database and redis skeletons

---
+++ servers/playbooks/postgresql/backup.daily.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+backupfile="daily-"`date "+%w"`".sql.gz"
+backuproot="/var/lib/pgsql/9.6/backups"
+mkdir -p $backuproot
+pg_dump -h db.mahaplatform.com -U maha maha | gzip > $backuproot/backup.sql.gz
+s3cmd put backup.sql.gz s3://data.mahaplatform.com/$backupfile
+rm -rf $backuproot/backup.sql.gz
