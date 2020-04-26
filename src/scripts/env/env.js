335992f9f60145b77dcf769d78f9002cf73cf598
Greg Kops <greg@thinktopography.com>
2020-04-26 09:59:31 -0400

    removing gsm and replace aws secrets manager

--- src/scripts/env/env.js
+++ src/scripts/env/env.js
@@ -1,47 +1,41 @@
 import '../../core/services/environment'
-import { SecretManagerServiceClient } from '@google-cloud/secret-manager'
+import aws from 'aws-sdk'
 import path from 'path'
 import ejs from 'ejs'
 import fs from 'fs'
 
+aws.config.constructor({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  region: process.env.AWS_REGION || ''
+})
+
 const env = async (root, environment) => {
 
-  const client = new SecretManagerServiceClient({
-    credentials: {
-      client_email: process.env.GOOGLE_GSM_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_GSM_PRIVATE_KEY
-    }
+  const client = new aws.SecretsManager({
+    region: process.env.AWS_REGION
   })
 
-  const [secrets] = await client.listSecrets({
-    parent: `projects/${ process.env.GOOGLE_GSM_PROJECTID }`
-  })
+  const secret = await client.getSecretValue({
+    SecretId: process.env.NODE_ENV
+  }).promise()
 
-  const envvars = await Promise.reduce(secrets, async(envvars, secret) => {
-
-    const [name] = secret.name.match(/([^/]*)$/)
-
-    const [version] = await client.accessSecretVersion({
-      name: `${secret.name}/versions/latest`
-    })
-
-    return {
-      ...envvars,
-      [name]: version.payload.data.toString()
-    }
-
-  }, {})
+  const envvars = JSON.parse(secret.SecretString)
 
   const getValue = (key) => {
-    return envvars[key] || ''
+    return envvars[key] ? envvars[key].replace(/\n/g, '\\n') : ''
   }
 
   const template = fs.readFileSync(path.join(__dirname, 'env.ejs'), 'utf8')
+
   const data = ejs.render(template, { getValue })
-  fs.writeFileSync(path.join(root,'.env'), data, 'utf8')
+
+  fs.writeFileSync(path.join(root, '.env'), data, 'utf8')
+
   if(process.env.CIRCLE_SHA1) {
-    fs.writeFileSync(path.join(root,'REVISION'), process.env.CIRCLE_SHA1, 'utf8')
+    fs.writeFileSync(path.join(root, 'REVISION'), process.env.CIRCLE_SHA1, 'utf8')
   }
+
 }
 
 export default env
