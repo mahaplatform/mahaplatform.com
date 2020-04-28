595a9dbd1a23622e8a76b79e6b36fd1d71876fb2
Greg Kops <greg@thinktopography.com>
2020-04-28 01:23:14 -0400

    programatically trigger contact activity on delivery

--- src/apps/crm/hooks/email/delivery.js
+++ src/apps/crm/hooks/email/delivery.js
@@ -1,11 +1,23 @@
 import { enrollInWorkflows } from '../../services/workflows'
+import { contactActivity } from '../../services/activities'
 
 const delivery = async (req, { email }) => {
 
   if(!email.get('email_campaign_id')) return
 
-  await email.load(['contact'], {
+  await email.load(['email_campaign','contact'], {
     transacting: req.trx
+  })
+
+  await contactActivity(req, {
+    contact: email.related('contact'),
+    type: 'email_campaign',
+    story: 'received an email campaign',
+    program_id: email.related('email_campaign').get('program_id'),
+    data: {
+      email_id: email.get('id'),
+      email_campaign_id: email.get('email_campaign_id')
+    }
   })
 
   await enrollInWorkflows(req, {
