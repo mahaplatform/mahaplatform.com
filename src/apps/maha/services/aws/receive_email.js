fa28340de812aae4642e62ee930b750330fd0042
Greg Kops <greg@thinktopography.com>
2020-05-11 11:39:21 -0400

    update mailboxes

--- src/apps/maha/services/aws/receive_email.js
+++ src/apps/maha/services/aws/receive_email.js
@@ -9,7 +9,7 @@ import Team from '../../models/team'
 import User from '../../models/user'
 import moment from 'moment'
 
-const mailboxes = collectObjects('hooks/email/*')
+const mailboxes = collectObjects('hooks/mailbox/*')
 
 const ADDRESS_REGEX = new RegExp(`^(.*)@(.*).${process.env.DOMAIN}$`)
 
