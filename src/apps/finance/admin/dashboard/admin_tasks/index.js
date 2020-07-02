534b29321d623cf233c0c9e938edaab76f812a23
Pat Haggerty <haggertypat@gmail.com>
2020-07-02 17:31:37 -0400

    first pass at finance admin tasks dashboard card

---
+++ src/apps/finance/admin/dashboard/admin_tasks/index.js
@@ -0,0 +1,10 @@
+import AdminTasks from './admin_tasks'
+
+const card = {
+  code: 'admin_tasks',
+  title: 'Finance Tasks',
+  description: 'Breakdown of tasks for Finance staff',
+  component: AdminTasks
+}
+
+export default card
---
+++ src/apps/finance/admin/dashboard/index.js
@@ -0,0 +1,7 @@
+const cards = [
+  {
+    code: 'admin_tasks'
+  }
+]
+
+export default cards
