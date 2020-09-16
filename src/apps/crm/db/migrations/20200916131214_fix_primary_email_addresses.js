f0da4196d7872bebb43bd40c100e0690994dff73
Greg Kops <greg@thinktopography.com>
2020-09-16 13:23:37 -0400

    trigger build

--- src/apps/crm/db/migrations/20200916131214_fix_primary_email_addresses.js
+++ src/apps/crm/db/migrations/20200916131214_fix_primary_email_addresses.js
@@ -1,6 +1,7 @@
 const FixPrimaryEmailAddresses = {
 
   up: async (knex) => {
+
     const emails = await knex.raw(`
       select e1.id
       from crm_contacts
@@ -8,11 +9,13 @@ const FixPrimaryEmailAddresses = {
       left join crm_email_addresses e2 on e2.contact_id=crm_contacts.id and e2.is_primary=true
       where e1.id is not null and e2.id is null
     `)
+
     await Promise.mapSeries(emails.rows, async (email) => {
       await knex('crm_email_addresses').where('id', email.id).update({
         is_primary: true
       })
     })
+
   },
 
   down: async (knex) => {
