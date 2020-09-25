52ba0e16b71b3f52409da7e448b97a665d227b2a
Greg Kops <greg@thinktopography.com>
2020-09-25 12:49:55 -0400

    show inactive on disabled projects

--- src/apps/finance/admin/tokens/project/index.js
+++ src/apps/finance/admin/tokens/project/index.js
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const ProjectToken = ({ integration, title }) => (
+const ProjectToken = ({ integration, is_active, title }) => (
   <div className="token project-token">
     { integration && integration.project_code && <strong>{ integration.project_code  } - </strong> }
     { title }
+    { is_active === false &&
+      <span className="compact-project-token-activity">(INACTIVE)</span>
+    }
   </div>
 )
 
 ProjectToken.propTypes = {
   integration: PropTypes.string,
+  is_active: PropTypes.bool,
   title: PropTypes.string
 }
 
--- src/apps/finance/serializers/membership_serializer.js
+++ src/apps/finance/serializers/membership_serializer.js
@@ -2,6 +2,7 @@ const MembershipSerializer = (req, result) => ({
   id: result.get('id'),
   display: result.get('display'),
   title: result.get('title'),
+  is_active: result.get('is_active'),
   integration: integration(req, result),
   member_type: result.get('member_type') ? result.get('member_type').toLowerCase() : 'member'
 })
