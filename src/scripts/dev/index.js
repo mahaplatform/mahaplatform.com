5ce5ee5b0d36214010c704f18484b1907939a25e
Greg Kops <greg@thinktopography.com>
2020-10-14 11:30:06 -0400

    update dev for admin servers

--- src/scripts/dev/index.js
+++ src/scripts/dev/index.js
@@ -149,7 +149,7 @@ const adminWatch = async () => {
       const adminRoot = path.join('src','core','admin','public')
       const parts = req.url.split('?').shift().split('/').slice(1)
       if(fs.existsSync(path.join(adminRoot,...parts))) return null
-      if(/^\/(api|auth|imagecache|oauth)/.test(req.url)) return null
+      if(/^\/(admin|api|imagecache)/.test(req.url)) return null
       if(/^\/notifications.js/.test(req.url)) return null
       return req.url
     }
