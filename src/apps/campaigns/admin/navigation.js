1bea7f6fd9309c2388aa2412addd48007938d28a
Greg Kops <greg@thinktopography.com>
2020-11-05 11:52:18 -0500

    fixing pathing for campaign module

--- src/apps/campaigns/admin/navigation.js
+++ src/apps/campaigns/admin/navigation.js
@@ -1,10 +1,10 @@
 const navigation = async (req, trx) => ({
   items: [
-    { label: 'Email', route: '/email', rights: ['crm:manage_email_campaigns'] },
-    { label: 'Inbound SMS', route: '/sms/inbound', rights: ['crm:manage_sms_campaigns'] },
-    { label: 'Inbound Voice', route: '/voice/inbound', rights: ['crm:manage_voice_campaigns'] },
-    { label: 'Outbound SMS', route: '/sms/outbound', rights: ['crm:manage_sms_campaigns'] },
-    { label: 'Outbound Voice', route: '/voice/outbound', rights: ['crm:manage_voice_campaigns'] }
+    { label: 'Email', route: '/email' },
+    { label: 'Inbound SMS', route: '/sms/inbound' },
+    { label: 'Inbound Voice', route: '/voice/inbound' },
+    { label: 'Outbound SMS', route: '/sms/outbound' },
+    { label: 'Outbound Voice', route: '/voice/outbound' }
   ]
 })
 
