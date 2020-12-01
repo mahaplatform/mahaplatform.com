68bfd109b98882a0200e2ad26a9a05a41aef7b18
Greg Kops <greg@thinktopography.com>
2020-12-01 15:19:07 -0500

    added filename token

--- src/apps/platform/admin/tokens/filename/index.js
+++ src/apps/platform/admin/tokens/filename/index.js
@@ -2,9 +2,13 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const FilenameToken = ({ original_file_name, source }) => (
-  <div className="token">
-    <img src={`/images/services/${source}.png`} width="16" height="16" />
-    { original_file_name }
+  <div className="filename-token">
+    <div className="filename-token-icon">
+      <img src={`/images/services/${source}.png`} width="16" height="16" />
+    </div>
+    <div className="filename-token-label">
+      { original_file_name }
+    </div>
   </div>
 )
 
