babeca9746630553a2b58d7aa797987119255772
Greg Kops <greg@thinktopography.com>
2020-12-24 11:32:08 -0500

    update serializer

--- src/apps/websites/serializers/next_serializer.js
+++ src/apps/websites/serializers/next_serializer.js
@@ -101,7 +101,7 @@ const nextSerializer = (req, result) => ({
                   fullWidth: false
                 },
                 spacing: {
-                  padding: [10, 10, 10, 10]
+                  padding: 10
                 },
                 background: {
                   background: {
@@ -322,6 +322,88 @@ const nextSerializer = (req, result) => ({
                       }
                     }
                   ]
+                }
+              }
+            ]
+          }
+        }, {
+          content: {
+            rows: [
+              {
+                sizing: {
+                  fullWidth: false
+                },
+                spacing: {
+                  padding: 10
+                },
+                content: {
+                  data: [
+                    { id: 1, photo: '/assets/91/img-20161125-wa0002.jpg', name: 'Aloja Airewele', position: 'Energy Warriors Coordinator', phone: '(607) 272-2292 x156', email: 'aaa247@cornell.edu' },
+                    { id: 2, photo: '/assets/3/ska2.jpg', name: 'Sharon Anderson', position: 'Environment Team Leader', phone: '(607) 272-2292 x156', email: 'ska2@cornell.edu' },
+                    { id: 3, photo: '/assets/5/kwb6.jpg', name: 'Karim Beers', position: 'Get Your Green Back Coordinator', phone: '(607) 272-2292 x186', email: 'kwb6@cornell.edu' },
+                    { id: 4, photo: '/assets/17/jrc10.jpg', name: 'Jennie Cramer', position: 'Horticulture Program Manager', phone: '(607) 272-2292 x146', email: 'jrc10@cornell.edu' },
+                    { id: 5, photo: '/assets/20/jd285.jpg', name: 'Josh Dolan', position: 'Food Gardening Outreach Educator', phone: '(607) 272-2292 x190', email: 'jd285@cornell.edu' },
+                    { id: 6, photo: '/assets/4727/juliana-garcia150x150.jpg', name: 'Juliana Garcia', position: 'Two Generation Family & Community Educator', phone: '(607) 272-2292 x115', email: 'jag394@cornell.edu' },
+                    { id: 7, photo: '/assets/62/kem228.jpg', name: 'Kenneth McLaurin, Jr.', position: 'Financial Management Educator', phone: '(607) 272-2292 x120', email: 'kem228@cornell.edu' }
+                  ],
+                  layout: {
+                    isResponsive: true,
+                    desktop: [3,3,3],
+                    tablet: [4,4,4],
+                    mobile: [6,6]
+                  },
+                  template: {
+                    animation: {
+                      type: 'fade',
+                      duration: 1000,
+                      delay: 0,
+                      opacity: 0,
+                      curve: 'ease-in-out',
+                      repeat: 1
+                    },
+                    background: {
+                      background: {
+                        hover: {
+                          type: 'color',
+                          color: '#EEEEEE'
+                        }
+                      }
+                    },
+                    border: {
+                      radius: 5
+                    },
+                    spacing: {
+                      padding: 20
+                    },
+                    content: {
+                      blocks: [
+                        {
+                          type: 'image',
+                          image: {
+                            border: {
+                              radius: '50%'
+                            }
+                          },
+                          caption: {
+                            sizing: {
+                              height: 130
+                            },
+                            text: {
+                              align: 'center'
+                            },
+                            spacing: {
+                              padding: 10
+                            }
+                          },
+                          content: {
+                            image: '<%- data.photo %>',
+                            caption: '<p><strong><%- data.name %></strong></p><p><%- data.position %></p><p><%- data.phone %></p><p><%- data.email %></p>',
+                            link: 'http://cornell.edu'
+                          }
+                        }
+                      ]
+                    }
+                  }
                 }
               }
             ]
