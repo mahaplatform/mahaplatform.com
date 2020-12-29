5e5c7deccfdc52ba12db47adb0ca43d75639c34a
Greg Kops <greg@thinktopography.com>
2020-12-29 04:13:46 -0500

    fixing sw

--- src/apps/websites/template/next.config.js
+++ src/apps/websites/template/next.config.js
@@ -1,18 +1,25 @@
 import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
-import withWorkbox  from 'next-with-workbox'
+import WorkboxPlugin from 'workbox-webpack-plugin'
 import withLess from '@zeit/next-less'
+import path from 'path'
 
-module.exports = withWorkbox(withLess({
-  workbox: {
-    swSrc: 'src/sw.js',
-    injectionPoint: 'self.__WB_MANIFEST'
-  },
+module.exports = withLess({
   webpack: (config, {dev, isServer}) => {
     config.node.fs = 'empty'
+    config.plugins.push(
+      new WorkboxPlugin.InjectManifest({
+        injectionPoint: 'self.__WB_MANIFEST',
+        swSrc: path.join(__dirname,'src','sw.js'),
+        swDest: path.join(__dirname,'public','sw.js'),
+        modifyURLPrefix: {
+          'static': '/_next/static'
+        }
+      })
+    )
     if(config.mode !== 'production') return config
     config.optimization.minimizer.push(
       new OptimizeCSSAssetsPlugin({})
     )
     return config
   }
-}))
+})
