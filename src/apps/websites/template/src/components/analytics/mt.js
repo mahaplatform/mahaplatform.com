b226bb73a4adb3d1371f1d23344a348ee61d71b7
Greg Kops <greg@thinktopography.com>
2021-01-10 11:26:42 -0500

    fix userid

--- src/apps/websites/template/src/components/analytics/mt.js
+++ src/apps/websites/template/src/components/analytics/mt.js
@@ -81,7 +81,7 @@ class MahaTracker {
       }
     })
     window.mt('enableActivityTracking', 30, 10)
-    this.setUser('abc')
+    this.setUser(1234)
     this.trackStructEvent('category', 'action', 'label', 'property', 12.2)
     this.trackSocialInteraction('action', 'network', 12.2)
     this.addItem('12345', 'sku', 'name', 'category', 10.00, 1, 'USD')
