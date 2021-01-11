21a2e0c3accf8d78d86c95d169c389618b57bc07
Greg Kops <greg@thinktopography.com>
2021-01-11 10:51:24 -0500

    remove pooling

--- src/core/vendor/knex.js
+++ src/core/vendor/knex.js
@@ -1,15 +1,9 @@
 import Knex from 'knex'
 
-const getPool = (env) => ({
-  min: (env === 'test') ? 1 : 5,
-  max: (env === 'test') ? 1 : 30
-})
-
 const knex = (connection) => new Knex({
   client: 'postgresql',
   connection,
-  useNullAsDefault: true,
-  pool: getPool(process.env.NODE_ENV)
+  useNullAsDefault: true
 })
 
 export const maha = knex(process.env.DATABASE_URL)
