226e6208a106f2d7c8174bc93b89e4cd74200ebb
Greg Kops <greg@thinktopography.com>
2021-01-11 11:08:55 -0500

    limit pool per server

--- src/core/vendor/knex.js
+++ src/core/vendor/knex.js
@@ -3,7 +3,11 @@ import Knex from 'knex'
 const knex = (connection) => new Knex({
   client: 'postgresql',
   connection,
-  useNullAsDefault: true
+  useNullAsDefault: true,
+  pool: {
+    min: 1,
+    max: 1
+  }
 })
 
 export const maha = knex(process.env.DATABASE_URL)
