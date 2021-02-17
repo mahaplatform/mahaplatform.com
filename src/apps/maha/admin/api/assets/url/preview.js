483eca89d68a7805d68cf98b9094e3b01b6ebdfc
Greg Kops <greg@thinktopography.com>
2021-02-17 11:19:33 -0500

    update to downloading assets by url

--- src/apps/maha/admin/api/assets/url/preview.js
+++ src/apps/maha/admin/api/assets/url/preview.js
@@ -6,8 +6,11 @@ const previewRoute = async (req, res) => {
 
     const response = await request.get(req.query.url, {
       resolveWithFullResponse: true,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
+      },
       encoding: null
-    })
+    }).promise()
 
     res.status(200).type(response.headers['content-type']).send(response.body)
 
--- src/apps/maha/admin/api/assets/url/show.js
+++ src/apps/maha/admin/api/assets/url/show.js
@@ -6,16 +6,24 @@ const urlRoute = async (req, res) => {
 
   try {
 
-    const response = await request.head(req.query.url).promise()
+    const response = await request.get(req.query.url, {
+      resolveWithFullResponse: true,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
+      },
+      encoding: null
+    }).promise()
 
     const parsed = Url.parse(req.query.url)
 
     res.status(200).respond({
-      ...response,
+      ...response.headers,
       file_name: path.basename(parsed.pathname)
     })
 
   } catch(e) {
+
+    console.log(e)
 
     res.status(404).respond({
       message: 'Unable to load url'
