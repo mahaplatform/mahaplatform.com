fc3bcff7f9daccc4472a07f96acab1d79a1658b1
Greg Kops <greg@thinktopography.com>
2021-02-25 11:36:47 -0500

    generalizing explorer solution to admin finder

--- src/apps/datasets/admin/api/datasets/destroy.js
+++ src/apps/datasets/admin/api/datasets/destroy.js
@@ -1,6 +1,54 @@
+import { activity } from '@core/services/routes/activities'
+import { audit } from '@core/services/routes/audit'
+import Dataset from '@apps/datasets/models/dataset'
+import socket from '@core/services/routes/emitter'
+import moment from 'moment'
+
 const destroyRoute = async (req, res) => {
 
-  res.status(200).respond()
+  const dataset = await Dataset.query(qb => {
+    qb.where('id', req.params.id)
+  }).fetch({
+    withRelated: ['types'],
+    transacting: req.trx
+  })
+
+  if(!dataset) return res.status(404).respond({
+    code: 404,
+    message: 'Unable to load dataset'
+  })
+
+  await Promise.mapSeries(dataset.related('types'), async (type) => {
+    await type.save({
+      deleted_at: moment()
+    }, {
+      transacting: req.trx,
+      patch: true
+    })
+  })
+
+  await dataset.save({
+    deleted_at: moment()
+  }, {
+    transacting: req.trx,
+    patch: true
+  })
+
+  await audit(req, {
+    story: 'deleted',
+    auditable: dataset
+  })
+
+  await activity(req, {
+    story: 'deleted {object}',
+    object: dataset
+  })
+
+  await socket.refresh(req, [
+    '/admin/datasets/datasets'
+  ])
+
+  res.status(200).respond(true)
 
 }
 
