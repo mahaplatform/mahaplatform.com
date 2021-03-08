a829e0aaa38e8ef6f67197b7a533de86a96e326a
Greg Kops <greg@thinktopography.com>
2021-03-07 21:59:57 -0500

    removing firebase from notification test

--- src/apps/maha/admin/api/notifications/test.js
+++ src/apps/maha/admin/api/notifications/test.js
@@ -1,31 +1,17 @@
-import { sendViaFirebase } from '@apps/maha/services/notifications/deliver'
 import { sendNotification } from '@apps/maha/services/notifications'
 
 const testRoute = async (req, res) => {
 
   await new Promise((rs) => setTimeout(rs, 1000))
 
-  if(req.body.type === 'firebase') {
-    await sendViaFirebase(req, {
-      user: req.user,
-      session: req.session,
-      device: req.device,
-      notification: {
-        subject: req.body.subject,
-        body: req.body.body,
-        route: req.body.route
-      }
-    })
-  } else {
-    await sendNotification(req, {
-      user: req.user,
-      notification: {
-        subject: req.body.subject,
-        body: req.body.body,
-        route: req.body.route
-      }
-    })
-  }
+  await sendNotification(req, {
+    user: req.user,
+    notification: {
+      subject: req.body.subject,
+      body: req.body.body,
+      route: req.body.route
+    }
+  })
 
   res.status(200).respond(true)
 
