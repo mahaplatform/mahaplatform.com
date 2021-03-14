5a5b72563adb815436c02f851bdbb7c45cbfb9e7
Greg Kops <greg@thinktopography.com>
2021-03-14 15:12:31 -0400

    dont load environment in test

--- src/core/services/environment.js
+++ src/core/services/environment.js
@@ -17,4 +17,4 @@ const paths = [
 
 const envPath = paths.find(path => fs.existsSync(path))
 
-if(envPath) dotenv.load({ path: envPath })
+if(process.env.NODE_ENV !== 'test' && envPath) dotenv.load({ path: envPath })
