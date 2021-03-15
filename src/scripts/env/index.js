36430f91d13f11d8d1ad0c7e53468b0dd42378a9
Greg Kops <greg@thinktopography.com>
2021-03-14 21:57:55 -0400

    fixing env step

--- src/scripts/env/index.js
+++ src/scripts/env/index.js
@@ -1,3 +1,10 @@
-import environment from './env'
+import path from './path'
+import env from './env'
+
+const environment = async () => {
+  const args = process.argv.slice(2)
+  const root = path.resolve('.')
+  await env(root, args[0])
+}
 
 environment().then(process.exit)
